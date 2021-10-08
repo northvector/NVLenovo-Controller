668fff7bf24f6bbb9ad2f5e69bdea78484fe3f44
Northvector <amirhosseinmehdipoor@gmail.com>
2021-10-08 13:14:04 +0330

    add a simple crash logger and auto restart

--- NVLenovoController/Program.cs
+++ NVLenovoController/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Security.Principal;
 using System.Threading.Tasks;
@@ -23,9 +25,52 @@ namespace NVLenovoController
         [STAThread]
         static void Main()
         {
+            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomainOnUnhandledException;
+
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new main_app());
+        }
+        private static void CurrentDomainOnUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            try
+            {
+                var ex = (Exception)e.ExceptionObject;
+
+                LogException(ex);
+            }
+            catch (Exception exception)
+            {
+                LogException(exception);
+            }
+        }
+
+        private static void LogException(Exception ex)
+        {
+            if(!File.Exists("log.txt"))
+            {
+                File.Create("log.txt");
+            }
+            var logFile = "log.txt";
+
+            using (FileStream stream = File.Open(logFile, FileMode.Append, FileAccess.Write,
+                FileShare.ReadWrite))
+            {
+                using (StreamWriter writer = new StreamWriter(stream))
+                {
+                    writer.WriteLine("Date: " + DateTime.Now);
+                    writer.WriteLine("Message: " + ex.Message);
+                    writer.WriteLine("StackTrace: " + ex.StackTrace);
+                }
+            }
+            Restart();
+        }
+        private static void Restart()
+        {
+            Process.Start(Application.ExecutablePath);
+            Application.Exit();
         }
     }
 }
